9176ecb3f7ab7d6f34b2b69a168821f0f9cb1850
antoniobfm <toinho111@gmail.com>
2023-08-06 21:32:58 -0300

    Fixed mistake on division by 0

--- exercicio1.py
+++ exercicio1.py
@@ -3,17 +3,18 @@
 # Crie a função que será avaliada no exercício aqui
 def multiplas_operacoes(x, y):
 
-  # Se y for igual a 0, retorne 0
-  if y == 0:
-    return 0
-
-  # Caso contrário, retorne a soma, subtração, multiplicação e divisão de x e y
+  # Soma, subtração, multiplicação e divisão de x e y
   soma = x + y
   subtracao = x - y
   multiplicacao = x * y
-  divisao = x / y
+
+  # Caso exista divisão por zero, retorne zero.
+  if y == 0:
+    divisao = 0
+  else:
+    divisao = x / y
 
   return soma, subtracao, multiplicacao, divisao
     
 # Teste a sua função aqui (caso ache necessário)
-print(multiplas_operacoes(10, 2))
+print(multiplas_operacoes(10, 0))
